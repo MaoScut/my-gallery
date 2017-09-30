016f5d01b8f6600c027ef5827be435e43b30f574
MaoScut <849345372@qq.com>
2017-09-30 18:02:38 +0800

    更新包，重新配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,40 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'app');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+const ROOT_PATH = path.resolve(__dirname);
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports = {
-	entry: {
-		app: path.resolve(APP_PATH, 'app.jsx')
-	},
-	output: {
-		path: BUILD_PATH,
-		filename: 'bundle.js'
-	},
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.jsx',
+  ],
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js',
+  },
 
-	devtool: 'eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    contentBase: './build',
+  },
 
-	devServer: {
-		historyApiFallback: true,
-		inline: true
-	},
-
-	module: {
-		rules: [{
-			test: /\.(js|jsx)$/,
-			enforce: "pre",
-			loader: "babel-loader",
-			include: APP_PATH
-		},
-		{
-			test: /\.(js|jsx)$/,
-			loader: 'babel-loader',
-			exclude: /node_modules/
-		},
-		{
-			test: /\.scss$/,
-			use: ["style-loader","css-loader","sass-loader"],
-			exclude: /node_modules/
-		},
-		{
-        test: /\.(png|jpg|gif|woff|woff2)$/,
-        loader: 'url-loader?limit=8192'
-        }],
-	},
-	// plugins: [
-	// 	new HtmlWebpackPlugin({
-	// 		title: 'just a test'
-	// 	})
-	// ],
-	resolve: {
-		extensions: ['.js', '.jsx']
-	}
-}
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      loader: ['babel-loader'],
+    },
+    {
+      test: /\.scss$/,
+      loader: ['style-loader', 'css-loader', 'sass-loader'],
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+  ],
+};
